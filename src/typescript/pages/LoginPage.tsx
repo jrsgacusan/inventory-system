09b67e6282ce06d8cfdba507ddc330c5d0c848d4
Juel Rei Gacusan <jgacusan@griddynamics.com>
2022-10-15 20:22:39 +0800

    Fix

---
+++ src/typescript/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import { styled } from '@mui/system';
+
+const Test = styled('div')`
+  width: 100%;
+  background-color: red;
+  color: red;
+`;
+
+const LoginPage = () => {
+  return <Test>This is the login page</Test>;
+};
+
+export default LoginPage;
